4190df35072b15287740dcd8a3840fcc5319f12a
Rage-100 <forcocgamegamil007@gmail.com>
2026-04-11 20:53:41 +0530

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,26 +1,44 @@
-#include <iostream>
-#include <ctime>
-#include <iomanip>
+#include <bits/stdc++.h>
 using namespace std;
 
-int n, arr1[100], arr2[100];
+void bubbleSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+}
 
-void print(int a[]) { for(int i=0;i<n;i++) cout<<a[i]<<" "; cout<<"\n"; }
-
-void bubble(int a[]) { for(int i=0;i<n-1;i++) for(int j=0;j<n-i-1;j++) if(a[j]>a[j+1]) swap(a[j],a[j+1]); }
-
-void insertion(int a[]) { for(int i=1;i<n;i++) { int k=a[i],j=i-1; while(j>=0&&a[j]>k) a[j+1]=a[j--]; a[j+1]=k; } }
-
-void run(void(*fn)(int[]), int a[], const string& name) {
-    clock_t s=clock(); fn(a); double t=(double)(clock()-s)/CLOCKS_PER_SEC;
-    cout<<"\n["<<name<<"] Sorted: "; print(a);
-    cout<<"Time: "<<fixed<<setprecision(10)<<t<<" sec | Time: O(n^2) | Space: O(1)\n";
+void insertionSort(vector<int>& arr) {
+    for (int i = 1; i < (int)arr.size(); i++) {
+        int key = arr[i], j = i - 1;
+        while (j >= 0 && arr[j] > key)
+            arr[j + 1] = arr[j--];
+        arr[j + 1] = key;
+    }
 }
 
 int main() {
-    cout<<"Enter number of elements: "; cin>>n;
-    cout<<"Enter "<<n<<" elements:\n";
-    for(int i=0;i<n;i++) { cin>>arr1[i]; arr2[i]=arr1[i]; }
-    run(bubble,    arr1, "Bubble Sort");
-    run(insertion, arr2, "Insertion Sort");
+    // Bubble Sort
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    vector<int> arr(n);
+    cout << "Enter " << n << " elements:\n";
+    for (int& x : arr) cin >> x;
+
+    bubbleSort(arr);
+    cout << "\nSorted array in ascending order:\n";
+    for (int x : arr) cout << x << " ";
+
+    // Insertion Sort
+    cout << "\n\nenter the values of array\n";
+    vector<int> arr2(5);
+    for (int& x : arr2) cin >> x;
+
+    insertionSort(arr2);
+    cout << "sorted ouptout\n";
+    for (int x : arr2) cout << "   " << x;
+
+    return 0;
 }
