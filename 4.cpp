3246b6363a302677671cb4d4608ce78c005f98fc
Rage-100 <forcocgamegamil007@gmail.com>
2026-04-11 20:56:34 +0530

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,35 +1,43 @@
-#include <iostream> 
-#include <queue> 
-using namespace std; 
-struct Node { char ch; int freq; Node *left, *right; Node(char c, int f) { ch = c; freq = f; left = 
-right = NULL; } }; 
-struct cmp { bool operator()(Node* a, Node* b) { return a->freq > b->freq; // min-heap } }; 
-void printCodes(Node* root, string code) { if (!root) return; 
-if (root->ch != '$')  // leaf node 
-    cout << root->ch << ": " << code << endl; 
- 
-printCodes(root->left, code + "0"); 
-printCodes(root->right, code + "1"); 
-  
-} 
-int main() { char ch[] = {'a','b','c','d'}; int freq[] = {5,9,12,13}; int n = 4; 
-priority_queue<Node*, vector<Node*>, cmp> pq; 
- 
-for(int i=0;i<n;i++) 
-    pq.push(new Node(ch[i], freq[i])); 
- 
-while(pq.size() > 1) { 
-    Node *l = pq.top(); pq.pop(); 
-    Node *r = pq.top(); pq.pop(); 
- 
-    Node *sum = new Node('$', l->freq + r->freq); 
-    sum->left = l; 
-    sum->right = r; 
- 
-    pq.push(sum); 
-} 
- 
-printCodes(pq.top(), ""); 
-return 0; 
-  
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    char data;
+    unsigned freq;
+    Node *left, *right;
+    Node(char d, unsigned f) : data(d), freq(f), left(nullptr), right(nullptr) {}
+};
+
+struct Compare {
+    bool operator()(Node* a, Node* b) { return a->freq > b->freq; }
+};
+
+void printCodes(Node* root, string code = "") {
+    if (!root) return;
+    if (!root->left && !root->right)
+        cout << root->data << ": " << code << "\n";
+    printCodes(root->left,  code + "0");
+    printCodes(root->right, code + "1");
+}
+
+int main() {
+    char arr[] = {'A','B','C','D','E','F'};
+    int  freq[] = {5, 9, 12, 13, 16, 45};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    priority_queue<Node*, vector<Node*>, Compare> pq;
+    for (int i = 0; i < size; i++)
+        pq.push(new Node(arr[i], freq[i]));
+
+    while (pq.size() > 1) {
+        Node *left = pq.top(); pq.pop();
+        Node *right = pq.top(); pq.pop();
+        Node *top = new Node('$', left->freq + right->freq);
+        top->left = left; top->right = right;
+        pq.push(top);
+    }
+
+    cout << "Huffman Codes are:\n";
+    printCodes(pq.top());
+    return 0;
 }
