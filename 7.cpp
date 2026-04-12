095577102f7b9061ed85c65f1c84f609138a91fb
Rage-100 <forcocgamegamil007@gmail.com>
2026-04-12 10:35:27 +0530

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -1,26 +1,17 @@
-#include <iostream> 
-#include <vector> 
-#include <string> 
-using namespace std; 
-  
-int LCS(const string &X, const string &Y) { 
-    int m = X.size(), n = Y.size(); 
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0)); 
-  
-    for (int i = 1; i <= m; i++) { 
-        for (int j = 1; j <= n; j++) { 
-            if (X[i - 1] == Y[j - 1]) 
-                dp[i][j] = dp[i-1][j-1] + 1; 
-            else 
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1]); 
-        } 
-    } 
-    return dp[m][n]; 
-} 
-  
-int main() { 
-    string X = "AGGTAB"; 
-    string Y = "GXTXAYB"; 
-    cout << "Length of LCS: " << LCS(X, Y) << endl; 
-    return 0; 
-} 
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char X[] = "AGGTAB", Y[] = "GXTXAYB";
+    int m = strlen(X), n = strlen(Y);
+    int dp[m+1][n+1];
+
+    for (int i = 0; i <= m; i++)
+        for (int j = 0; j <= n; j++)
+            dp[i][j] = (i==0 || j==0) ? 0 :
+                       (X[i-1]==Y[j-1] ? dp[i-1][j-1]+1
+                                       : max(dp[i-1][j], dp[i][j-1]));
+
+    cout << "Length of LCS is " << dp[m][n];
+}
