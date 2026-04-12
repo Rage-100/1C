69ecf5fdbc9e816658f9ffffb801287128b19ac0
Rage-100 <forcocgamegamil007@gmail.com>
2026-04-12 10:11:40 +0530

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,89 +1,87 @@
-#include <bits/stdc++.h> using namespace std; 
-#define P pair<int,int> 
-// ---------- Prim's Algorithm ---------- int prim(int V, vector<vector 
->& adj) { priority_queue<P, vector 
-, greater 
-> pq; vector visited(V, false); 
-pq.push({0, 0}); 
-int cost = 0; 
- 
-while (!pq.empty()) { 
-    auto [wt, u] = pq.top(); 
-    pq.pop(); 
- 
-    if (visited[u]) continue; 
- 
-    visited[u] = true; 
-    cost += wt; 
- 
-    for (auto [v, w] : adj[u]) { 
-        if (!visited[v]) 
-            pq.push({w, v}); 
-    } 
-} 
-return cost; 
-  
-} 
-// ---------- Kruskal's Algorithm ---------- struct Edge { int u, v, w; }; 
-vector parent, rankv; 
-int findSet(int x) { if (parent[x] != x) parent[x] = findSet(parent[x]); return parent[x]; } 
-void unionSet(int a, int b) { a = findSet(a); b = findSet(b); 
-if (a != b) { 
-    if (rankv[a] < rankv[b]) 
-        swap(a, b); 
-    parent[b] = a; 
-    if (rankv[a] == rankv[b]) 
-        rankv[a]++; 
-} 
-  
-} 
-int kruskal(int V, vector& edges) { sort(edges.begin(), edges.end(), [](Edge a, Edge b) { return 
-a.w < b.w; }); 
-parent.resize(V); 
-rankv.resize(V, 0); 
- 
-for (int i = 0; i < V; i++) 
-    parent[i] = i; 
- 
-int cost = 0; 
- 
-for (auto e : edges) { 
-    if (findSet(e.u) != findSet(e.v)) { 
-        unionSet(e.u, e.v); 
-        cost += e.w; 
-    } 
-} 
-return cost; 
-  
-} 
-// ---------- Main Function ---------- int main() { int V = 4; 
-// Adjacency list for Prim 
-vector<vector<P>> adj(V); 
- 
-adj[0].push_back({1, 10}); 
-adj[1].push_back({0, 10}); 
- 
-adj[0].push_back({2, 6}); 
-adj[2].push_back({0, 6}); 
- 
-adj[0].push_back({3, 5}); 
-adj[3].push_back({0, 5}); 
- 
-adj[1].push_back({3, 15}); 
-adj[3].push_back({1, 15}); 
- 
-adj[2].push_back({3, 4}); 
-adj[3].push_back({2, 4}); 
- 
-// Edge list for Kruskal 
-vector<Edge> edges = { 
-    {0,1,10}, {0,2,6}, {0,3,5}, 
-    {1,3,15}, {2,3,4} 
-}; 
- 
-cout << "Prim MST cost: " << prim(V, adj) << endl; 
-cout << "Kruskal MST cost: " << kruskal(V, edges) << endl; 
- 
-return 0; 
-  
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+
+using namespace std;
+
+#define INF 9999
+#define MAX 100
+
+struct Edge {
+    int u, v, w;
+};
+
+// --- UNION-FIND (Kruskal) ---
+int find(vector<int>& parent, int i) {
+    return (parent[i] == i) ? i : parent[i] = find(parent, parent[i]);
+}
+
+// --- PRIM'S ALGORITHM ---
+void primMST(int cost[MAX][MAX], int n) {
+    int selected[MAX] = {0}, no_edge = 0, totalCost = 0;
+    selected = 1;
+    cout << "\nPrim’s Algorithm Edges:\n";
+    
+    while (no_edge < n - 1) {
+        int minW = INF, x = 0, y = 0;
+        for (int i = 0; i < n; i++)
+            if (selected[i])
+                for (int j = 0; j < n; j++)
+                    if (!selected[j] && cost[i][j] < minW) {
+                        minW = cost[i][j]; x = i; y = j;
+                    }
+        cout << "Edge " << ++no_edge << ": (" << x << " - " << y << ") cost: " << minW << endl;
+        totalCost += minW;
+        selected[y] = 1;
+    }
+    cout << "Total cost using Prim’s Algorithm = " << totalCost << endl;
+}
+
+// --- KRUSKAL'S ALGORITHM ---
+void kruskalMST(int cost[MAX][MAX], int n) {
+    vector<Edge> edges;
+    for (int i = 0; i < n; i++)
+        for (int j = i + 1; j < n; j++)
+            if (cost[i][j] != INF) edges.push_back({i, j, cost[i][j]});
+
+    sort(edges.begin(), edges.end(), [](Edge a, Edge b) { return a.w < b.w; });
+
+    vector<int> parent(n);
+    for (int i = 0; i < n; i++) parent[i] = i;
+
+    int totalCost = 0, edgeAdded = 0;
+    cout << "\nKruskal’s Algorithm Edges:\n";
+    for (auto &e : edges) {
+        int uR = find(parent, e.u), vR = find(parent, e.v);
+        if (uR != vR) {
+            cout << "Edge " << ++edgeAdded << ": (" << e.u << " - " << e.v << ") cost: " << e.w << endl;
+            totalCost += e.w;
+            parent[uR] = vR;
+            if (edgeAdded == n - 1) break;
+        }
+    }
+    cout << "Total cost using Kruskal’s Algorithm = " << totalCost << endl;
+}
+
+int main() {
+    int n, cost[MAX][MAX];
+    cout << "Enter number of vertices: "; cin >> n;
+    cout << "Enter the cost adjacency matrix (use 0 for no edge):\n";
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++) {
+            cin >> cost[i][j];
+            if (cost[i][j] == 0 && i != j) cost[i][j] = INF;
+        }
+
+    clock_t start = clock();
+    primMST(cost, n);
+    double pT = double(clock() - start) / CLOCKS_PER_SEC;
+
+    start = clock();
+    kruskalMST(cost, n);
+    double kT = double(clock() - start) / CLOCKS_PER_SEC;
+
+    printf("\n⏱️ Execution Time Comparison:\nPrim’s Algorithm Time: %.10f seconds\nKruskal’s Algorithm Time: %.10f seconds\n", pT, kT);
+    return 0;
 }
