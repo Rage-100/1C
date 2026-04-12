e9dd6f0fc97bf10e2934bfd2050eecdd93178f51
Rage-100 <forcocgamegamil007@gmail.com>
2026-04-12 10:34:15 +0530

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -1,27 +1,35 @@
-#include <iostream> 
-#include <vector> 
-#include <algorithm> 
-using namespace std; 
-  
-int knapsack(int W, vector<int>& wt, vector<int>& val) { 
-    int n = wt.size(); 
-    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0)); 
-  
-    for (int i = 1; i <= n; i++) { 
-        for (int w = 0; w <= W; w++) { 
-            if (wt[i-1] <= w) 
-                dp[i][w] = max(dp[i-1][w], dp[i-1][w - wt[i-1]] + val[i-1]); 
-            else 
-                dp[i][w] = dp[i-1][w]; 
-        } 
-    } 
-    return dp[n][W]; 
-} 
-  
-int main() { 
-    vector<int> values = {60, 100, 120}; 
-    vector<int> weights = {10, 20, 30}; 
-    int W = 50; 
-    cout << "Maximum value: " << knapsack(W, weights, values) << endl; 
-    return 0; 
-} 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, W;
+    cout << "Enter number of items: ";
+    cin >> n;
+
+    int val[n], wt[n];
+    cout << "Enter values (profits) of " << n << " items:\n";
+    for (int i = 0; i < n; i++) cin >> val[i];
+
+    cout << "Enter weights of " << n << " items:\n";
+    for (int i = 0; i < n; i++) cin >> wt[i];
+
+    cout << "Enter capacity of Knapsack: ";
+    cin >> W;
+
+    int K[n + 1][W + 1];
+
+    for (int i = 0; i <= n; i++)
+        for (int w = 0; w <= W; w++)
+            K[i][w] = (i == 0 || w == 0) ? 0 :
+                      (wt[i - 1] <= w ? max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w])
+                                      : K[i - 1][w]);
+
+    cout << "\nDynamic Programming Table:\n";
+    for (int i = 0; i <= n; i++) {
+        for (int w = 0; w <= W; w++)
+            cout << K[i][w] << " ";
+        cout << "\n";
+    }
+
+    cout << "\nMaximum value that can be put in the knapsack = " << K[n][W] << endl;
+}
